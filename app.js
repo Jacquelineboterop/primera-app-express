6a3dc95ec15b826ce5bb90245c04d92a37f5092e
Jacqueline <proyectos@branta.com.co>
2021-05-13 16:32:16 -0500

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('<h1>HcUy6Re2LLBRtj</h1>');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log('Listening on port 3000!'));*/
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => { 
+   if(req.query.nombre != null) 
+  { res.send('Hola ' + req.query.nombre);   
+   }
+     else { res.send('Hola desconocido');
+          }    });
+  app.listen(3000, () => console.log('Listening on port 3000!'));
