478a603d729506a05388e779becdcf6366957de2
Jacqueline <proyectos@branta.com.co>
2021-05-13 16:45:01 -0500

    saludame1

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => { 
    if(req.query.nombre != null) 
-  { res.send('Hola ' + req.query.nombre);   
+  { res.send('<h1>Hola ' + req.query.nombre</h1>);   
    }
-     else { res.send('Hola desconocido');
+     else { res.send('<h1>Hola desconocido</h1>');
           }    });
   app.listen(3000, () => console.log('Listening on port 3000!'));
