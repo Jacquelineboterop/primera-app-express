3281fcb2eb3ef4573bcf62eb117372bcbc2580d6
Jacqueline <proyectos@branta.com.co>
2021-05-13 16:49:10 -0500

    saludame1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => { 
    if(req.query.nombre != null) 
-  { res.send('<h1>Hola </h1>' + req.query.nombre);   
+  { res.send('<h1>Hola Juan</h1>' + req.query.nombre);   
    }
      else { res.send('<h1>Hola desconocido</h1>');
           }    });
