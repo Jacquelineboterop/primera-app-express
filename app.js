dbaff68f36f35a1d1429b6307fc00c25221bdb65
Jacqueline <proyectos@branta.com.co>
2021-05-18 20:42:28 -0500

    header

--- app.js
+++ app.js
@@ -63,14 +63,14 @@ var server=app.listen(3000, () => {
 }); */
 
 
-var express = require('express');
-var app = express();
-var useragent = require('express-useragent');
- 
-app.use(useragent.express());
-app.get('/', function(req, res){
-  
-    res.send(req.useragent.browser);
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => { 
+  res.send(  req.header('User-Agent')
+  );   
 });
-app.listen(3000);
+app.listen(3000, () => console.log('Listening on port 3000!'));
 
