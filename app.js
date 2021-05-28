ab1919a263d1e0411a472c635007ec6cbb67e3ed
Jacqueline <proyectos@branta.com.co>
2021-05-27 20:39:25 -0500

    first commit

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.post('/mostrartexto', (req, res) => {
 
 var server=app.listen(3000, () => {
   console.log('Servidor web iniciado');
-}); */
+}); 
 
 
 const express = require('express');
@@ -72,5 +72,47 @@ app.get('/', (req, res) => {
   res.send(  req.header('User-Agent')
   );   
 });
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log('Listening on port 3000!')); 
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost/EmployeeDB';
+
+MongoClient.connect(url, function(err, db) {
+
+      cursor.log("connected"); 
+
+        db.close();
+
+    }); 
+
+    const consecutive = arr => 
+    arr.find((element, index) => element - arr[index - 1] > 1 )||  null;
+      
+    console.log(consecutive([1,2,3,5])); 
+
+    const sumsequence = arr => {
+      if (!arr[0]) return 0
+
+      const lastNumber = arr[arr.length - 1];
+      return arr.filter((element, index) => index % 2 ===0).reduce((acum, element) => acum + element) * lastNumber;
+
+    }
+    console.log(sumsequence([]));*/
+    const express = require('express'); 
+    const app = express(); 
+    const port = 3000;     
+    var mongoose = require("mongoose"); 
+    mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true, useUnifiedTopology: true })  
+    mongoose.connection.on("error", function(e) { 
+    console.error(e); 
+      });  
+    const schema = mongoose.Schema({   date: Date,   name: { type: String, default: "Anónimo" } });  
+    const Visitor = mongoose.model("Visitor", schema);  app.get('/', (req, res) => {   const nombre = req.query.name;     
+    Visitor.create(     {
+                           name: nombre,       date: new Date()     }, (error) => {       
+                             if (error){         
+                               return;       
+                              }       
+                           res.send('<h1>El visitante fue almacenado con éxito</h1>');     
+                          }   ) });   
+                          app.listen(3000);
